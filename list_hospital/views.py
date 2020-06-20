7fc73f9699832a1cd29189bea211170564852b7d
Ravi <jravi248@gmail.com>
2020-06-20 15:08:52 +0530

    Add basic views for list_hospital app

--- list_hospital/views.py
+++ list_hospital/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .APIs import find_place, check
+from accounts.models import Patient
+from .chatbot import *
+import json
+import time
 
-# Create your views here.
+
+def search(request):
+    if request.POST:
+        return redirect("list_hospitals", {"data": request.POST["data"]})
+    else:
+        return render(request, "list_hospitals/search.html", {})
+
+
+def list_hospitals(request, disease):
+    if disease:
+        # Generate all nearby facilities'
+        # print(request.GET)
+        print(str(disease).strip())
+        hospitals = check(str(disease), 28.630999, 77.372165)
+    else:
+        # Generate Customised facilities
+        hospitals = find_place(28.6358749, 77.3738937, ["clinic"])
+    return render(request, "list_hospitals/index.html", {"hospitals": hospitals})
