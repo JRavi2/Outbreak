a79ecb3c61b38de26a09f3eed2ef760ef6c6af4f
Ravi <jravi248@gmail.com>
2020-06-21 15:34:50 +0530

    Minor Bug Fixes

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout/', djViews.LogoutView.as_view(), name='logout'),
     path('signup/patient/', views.signup_p, name='signup_p'),
     path('signup/hospital/', views.signup_h, name='signup_h'),
+    path('gotodashboard', views.goto_dashboard, name='dashboard'),
     path('pat/dashboard/', views.patient_dashboard, name='patient_dashboard'),
     path('hos/dashboard/', views.hospital_dashboard, name='hospital_dashboard'),
     path('hos/upd-tokens/', views.update_tokens, name='update_tokens'),
--- hackjaipur/settings.py
+++ hackjaipur/settings.py
@@ -85,7 +85,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 
